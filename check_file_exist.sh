64201f898a7a8b4be7cffde7504861eb931bf95a
ra-code422 <rafiatousoumailahamidou@gmail.com>
2025-05-25 02:34:36 +0100

    Add files via upload

---
+++ check_file_exist.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Script pour vérifier si le fichier sample.txt existe
+
+fichier="sample.txt"
+
+if [ -f "$fichier" ]; then
+    echo "Le fichier '$fichier' existe."
+else
+    echo "Le fichier '$fichier' n'existe pas."
+fi
